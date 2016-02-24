642d91daf6dc174ee70e4c2f624024bc0b9ff25e
Xiaopeng Liu <xiaopeng.liu@thomsonreuters.com>
2016-02-24 23:28:09 +0800

    bat usage for myself

---
+++ tdaccelarator/GoAccelarate.bat
@@ -0,0 +1,4 @@
+D:
+cd D:\Work2\TDAccelarator\tdaccelarator\target
+java -jar tdaccelarator-0.0.1-SNAPSHOT-jar-with-dependencies.jar "C:/Program Files (x86)/Thunder Network/Thunder/Profiles/TaskDB.dat" AccelerateTaskMap27566689_superspeed_1_1
+pause
