55b657447474ff91ed16623f692dbaa832684401
XPXPXP <liuxp87@gmail.com>
2016-11-07 15:55:18 +0800

    Add 2nd line

--- tdaccelarator/src/main/java/xpxpxp/top/tdaccelarator/App.java
+++ tdaccelarator/src/main/java/xpxpxp/top/tdaccelarator/App.java
@@ -34,6 +34,7 @@ public class App
 //	    	table = "AccelerateTaskMap27566689_superspeed_1_1";
 
     	//Add this line for Hygieia test
+	//2 Add this line for Hygieia test
         
         Connection connection = null;
         
